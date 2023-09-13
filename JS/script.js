168ecfd0f8703baa6828b802f4000f01be46c7a5
Aun-Annop <annop.sukprasert@gmail.com>
2023-09-13 17:35:52 +0700

    cod2

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+/*================================typing animation======================= */
+var typed = new Typed(".typing",{
+    strings:["","Web Desinger","Graphic Designer","Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
